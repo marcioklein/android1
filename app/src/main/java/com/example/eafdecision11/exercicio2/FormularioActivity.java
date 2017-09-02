ea6a1e5ceeae1944bf95deea7c326ab9d7faa74f
Marcio Klein <marcioklein@gmail.com>
2017-09-02 17:03:40 -0300

    aula android

---
+++ app/src/main/java/com/example/eafdecision11/exercicio2/FormularioActivity.java
@@ -0,0 +1,20 @@
+package com.example.eafdecision11.exercicio2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+
+    public void proximaTela(View v){
+        Intent intent = new Intent(this, OptionsFormActivity.class);
+        startActivity(intent);
+    }
+}
